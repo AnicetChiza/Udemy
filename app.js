4bae641fc954267afabeb5b3d5e0a4eea197e47c
unknown <chizaanicet@gmail.com>
2023-11-18 15:26:03 +0200

    add to my repository

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ document.querySelector('.score').textContent = 13;
 document.querySelector('.guess').value = 23;
 document.querySelector('.number').textContent = 23;*/
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.btn').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
+
     if(!guess){
-        document.querySelector('.message').textContent = 'No number entered';
+        document.querySelector('.message').textContent = '🔥 No number';
+    }
+
+    else if(guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct number';
+    }
+
+    else if(guess > secretNumber){
+        document.querySelector('.message').textContent = '📈 To high';
+    }
+
+    else if(guess < secretNumber){
+        document.querySelector('.message').textContent = '📉 Too lower';
     }
 });

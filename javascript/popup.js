f111f52e1e8e07324d282cf394c35b21840635e6
unknown <chizaanicet@gmail.com>
2023-11-19 14:20:03 +0200

    add to my repository,

---
+++ javascript/popup.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/*const modal = document.querySelector('.modal');
+const closeButton = document.querySelector('.modal-close');
+const overAll = document.querySelector('.overall');
+const btnOpenModal = document.querySelectorAll('.button');
+
+console.log(btnOpenModal);
+for( let i=0; i = btnOpenModal.length; i++);*/

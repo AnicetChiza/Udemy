0453c8d81cafbe8953a4905aa9f8964f7271d264
unknown <chizaanicet@gmail.com>
2023-12-02 12:21:55 +0200

    add to my repository

--- javascript/popup.js
+++ javascript/popup.js
@@ -4,14 +4,23 @@
 const modal = document.querySelector('.modal');
 const btnCloseModal = document.querySelector('.btn-close');
 const btnOpenModal = document.querySelectorAll('.button');
-const overAll = document.querySelector('.overall');
+const overall = document.querySelector('.overall');
 
 console.log(btnOpenModal);
 
 for(let i = 0; i < btnOpenModal.length; i++)
     btnOpenModal[i].addEventListener('click', function(){
         modal.classList.remove('hidden');
-        overAll.classList.remove('hidden');
+        overall.classList.remove('hidden');
 });
+
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overall.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overall.addEventListener('click', closeModal);
 
 

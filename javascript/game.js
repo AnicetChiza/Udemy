1f58da3633a33999e53a950978ea00c1a183c578
unknown <chizaanicet@gmail.com>
2023-12-03 13:24:41 +0200

    add to my repository

--- javascript/game.js
+++ javascript/game.js
@@ -10,6 +10,7 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+
 //Stating conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -24,7 +25,7 @@ btnRoll.addEventListener('click', function() {
 
     //2. Display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice${dice}.png`;
+    diceEl.src = `dice-${dice}.png`;
 
     //Check for rolled 1
     if(dice !== 1){

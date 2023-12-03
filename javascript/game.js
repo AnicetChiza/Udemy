7a70712a6988409dca9c82076469ead905daa075
unknown <chizaanicet@gmail.com>
2023-12-03 11:56:48 +0200

    add to my repository

--- javascript/game.js
+++ javascript/game.js
@@ -3,6 +3,8 @@
 //SELECTING AND CALLING ELEMENTS
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const currentEl0 = document.querySelector('.current--0');
+const currentEl1 = document.querySelector('.current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -13,7 +15,21 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden'); 
 
+let currentScore = 0;
+
 //Rolling dice functions
 btnRoll.addEventListener('click', function() {
-    
+    //1. Generating a random dice
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    //2. Display dice
+    diceEl.classList.remove('hidden');
+    dice.src = `dice-${dice}.png`;
+
+    //Check for rolled 1
+    if(dice !== 1){
+        //Add dice to the current score
+        currentScore += dice;
+        currentEl0.textContent = currentScore;
+    }
 })
